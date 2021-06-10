7ac6c0213371ef131bc2ecd3d820ed80b357e3a9
Kentucky94 <lis5043@gmail.com>
2021-06-10 17:30:29 +0600

    initial commit

---
+++ markup/script.js
@@ -0,0 +1,41 @@
+const btn = document.getElementById("mobile-menu-button");
+
+btn.onclick = function () {
+    const menu = document.getElementById("mobile-menu");
+
+    if (menu.classList.contains("open")) {
+        menu.classList.remove("open")
+    } else {
+        menu.classList.add("open")
+    }
+};
+
+const cardButtons = document.getElementsByClassName("options-block_cards-select-button");
+const cardsBlock = document.getElementsByClassName('options-block_cards')[0];
+
+for (let i = 0; i < cardButtons.length; i++) {
+    const btn = cardButtons[i];
+
+    btn.onclick = event => {
+        const element = event.currentTarget;
+        removeActive();
+        element.classList.add('active');
+        const target = element.dataset.target;
+        const selector = `[data-card="${target}"]`;
+        const card = document.querySelector(selector); // card-div
+        const shift = -1 * card.offsetLeft;
+
+        cardsBlock.style.transform = `translateX(${shift}px)`;
+    }
+}
+
+const removeActive = () => {
+    for (let i = 0; i < cardButtons.length; i++) {
+        const btn = cardButtons[i];
+        btn.classList.remove('active');
+    }
+};
+
+window.onresize = function () {
+    cardsBlock.style.transform = `translateX(0)`;
+};
